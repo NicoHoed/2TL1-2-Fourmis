6f9253d67ba824325a1f2ddba8492da9d18efd60
P-A <p.hausman@students.ephec.be>
2024-10-15 17:44:03 +0200

    add docstring

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from random import randint
+
+
 class Colony:
+    """
+    a class for representing a Colony
+    """
     def __init__(self, ct_ant, food_stock):
         self.ctAnt = ct_ant
         self.food_stock = food_stock
@@ -14,6 +20,9 @@ class Colony:
 
 
 class Ant:
+    """
+    a class for representing a Ant
+    """
     def __init__(self, role):
         self.position = [0, 0]
         self.role = role
@@ -24,8 +33,27 @@ class Ant:
     def detect_pheromone(self):
         pass
 
+    @property
+    def x(self):
+        return self.position[0]
+
+    @x.setter
+    def x(self, x_pos):
+        self.position[0] = x_pos
+
+    @property
+    def y(self):
+        return self.position[1]
+
+    @y.setter
+    def y(self, y_pos):
+        self.position[1] = y_pos
+
 
 class Queen(Ant):
+    """
+    a class for representing the unique queen
+    """
     def __init__(self):
         super().__init__('queen')
         pass
@@ -35,6 +63,9 @@ class Queen(Ant):
 
 
 class Worker(Ant):
+    """
+    class for representing an ant of type worker
+    """
     def __init__(self):
         super().__init__('worker')
         pass
@@ -47,6 +78,9 @@ class Worker(Ant):
 
 
 class Soldier(Ant):
+    """
+        class for representing an ant of type soldier
+        """
     def __init__(self):
         super().__init__('soldier')
         pass
@@ -56,6 +90,9 @@ class Soldier(Ant):
 
 
 class Pheromone:
+    """
+    a class for representing a pheromone
+    """
     def __init__(self, p_type, intensity):
         self.p_type = p_type
         self.intensity = intensity
@@ -65,6 +102,9 @@ class Pheromone:
 
 
 class Nest:
+    """
+    a class for representing a nest
+    """
     def __init__(self, capacity):
         self.capacity = capacity
 
@@ -72,3 +112,14 @@ class Nest:
         pass
 
 
+
+ant_dict = []
+for x in range(50):
+    ant = Worker()
+    ant.position = [randint(0, 100), randint(0, 100)]
+    ant.x += 200
+    ant.y += 100
+    ant_dict.append(ant)
+
+for x in ant_dict:
+    print(x.role, x.x, x.y)
