bf885887a0e7291ad52ac4a96210c8c263af5c0b
P-A <he202408@students.ephec.be>
2024-10-31 16:27:28 +0100

    enhanced lib

--- lib/monsterCreation.py
+++ lib/monsterCreation.py
@@ -1,23 +1,25 @@
 from json import dump
 from os import path
 
-def create_monster(dir, name, life, min_nest_level, spawn_prob):
+keys = ['name', 'life', 'min_nest_level', 'spawn_prob']
+
+def create_monster(directory: str, *param: list):
+    global keys
     # Creation of the monster dictionary
-    monster = {
-        "name": name,
-        "life": life,
-        "min_nest_level": min_nest_level,
-        "spawn_prob": spawn_prob
-    }
+    monster = {}
+    for key in range(len(keys)):
+        monster[keys[key]] = param[key]
 
 
 
     # Save the monster in a JSON file
-    with open(path.join(dir , f"{name}.json"), "w", encoding='utf-8') as file:
+    with open(path.join(directory , f"{monster['name']}.json"), "w", encoding='utf-8') as file:
         dump(monster, file, indent=4)
 
-    print(f"The monster {name} was created successfully !")
+    print(f"The monster {monster['name']} was created successfully !")
     print(monster)
+
+
 
 
 if __name__ == '__main__':
